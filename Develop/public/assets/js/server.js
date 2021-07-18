780ccda164b8770d396378adb6ba31711423890a
Ishya Casukhela <casukhelai@gmail.com>
2021-07-18 19:41:54 -0400

    made sure server was running and creating skeleton for get requests

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,7 +1,9 @@
 // set up the server
+// USE "node server.js" IN PATH /js TO RUN SERVER
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const noteList = JSON.parse(data);
 
 // set up Express app
 const PORT = process.env.PORT || 3001;
@@ -16,10 +18,18 @@ app.use(express.json());
 // ***************
 
 // Set up API routes
-module.exports = (app) => {
 // Following will be setting up /api/notes
 // need to allow the user to set up the API somehow
-fs.readFile
+const readFile = (note) => {
+    fs.readFile(
+        path.join(__dirname, "db/db.json"),
+        // parse json into stringify just in case
+        JSON.stringify(note),
+        // throw an error message
+        err => (err ? console.log(err) : console.log("File saved!"))
+    );
+}
+
 // create something such that the user sets up the notes variable
 
 // GET requests
@@ -27,9 +37,9 @@ fs.readFile
 
 
 // POST requests
-}
 
 
+ 
 // add app.listen
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
