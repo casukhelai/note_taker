fce35094a5d658044ee1b9e235fe01cdb8145d6f
Ishya Casukhela <casukhelai@gmail.com>
2021-07-19 13:12:15 -0400

    created new server since original server dot js file was having an error...this is a test to see which to debug first

---
+++ Develop/public/assets/js/server2.js
@@ -0,0 +1,76 @@
+// set up the server
+// USE "node server.js" IN PATH /js TO RUN SERVER
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+
+// set up Express app
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// add middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// ***************
+//   API Route
+// ***************
+
+module.exports = (app) => {
+
+// Set up JSON API file
+fs.readFile("db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+
+    var notes = JSON.parse(data);
+    // create something such that the user sets up the notes variable
+
+// ****************************************************************************
+//   GET requests -- make the appropriate 'connections' to all relevant files
+// ****************************************************************************
+
+// default link when an invalid request is made
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+// request to the notes html file
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+// notes json api route
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+});
+
+
+
+// ****************************************************************************
+//   POST requests -- create appropriate 'destinations' for the GET requests
+// ****************************************************************************
+
+// POST Request after note is "saved"
+app.post('/api/notes', (req,res) => {
+    let newNote = req.body;
+    notes.push(newNote);
+    createNotes();
+    console.log("test");
+});
+
+
+
+function createNotes() {
+    fs.writeFile("db/db.json",JSON.stringify(notes),
+    (err) => (err ? console.log(err): console.log("File saved!")));
+}
+})
+
+
+}
+
+// add app.listen
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+
